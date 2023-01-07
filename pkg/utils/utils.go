d8f373c944c5a67fdb2c587a2b57b60e3f5fcbc1
hramov <trykhramov@gmail.com>
2023-01-07 16:11:46 +0700

    Cleaning

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,21 +1,5 @@
 package utils
 
-import appError "github.com/hramov/tg-bot-admin/internal/error"
-
-type AppResponse[T comparable] struct {
-	Status bool
-	Error  appError.IAppError
-	Data   *T
-}
-
-func CreateAppResponse[T comparable](status bool, err appError.IAppError, data *T) *AppResponse[T] {
-	return &AppResponse[T]{
-		Status: status,
-		Error:  err,
-		Data:   data,
-	}
-}
-
 func EqualSlice(a, b []uint8) bool {
 	if len(a) != len(b) {
 		return false
